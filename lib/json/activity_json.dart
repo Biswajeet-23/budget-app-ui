bdb09bfbb61070f35ad6ffa1460ba171665bcf44
Biswajeet Sahoo <2000biswajeet@gmail.com>
2023-10-18 09:26:59 +0530

    initial commit

---
+++ lib/json/activity_json.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_font_icons/flutter_font_icons.dart';
+
+const List  items = [
+      {
+        "title" : "Transfer",
+        "icon" : Feather.send
+      },
+       {
+        "title" : "My Card",
+        "icon" : AntDesign.creditcard
+      },
+       {
+        "title" : "Insight",
+        "icon" : AntDesign.areachart
+      }
+    ];
